b22291d52324466c4cd466ae25b7183d96de1f74
Francois Travais <francois.travais@gmail.com>
2016-03-13 18:29:12 +0000

    First working version

--- blog/lib/index.js
+++ blog/lib/index.js
@@ -2,11 +2,75 @@
  * Lib
  */
 
-module.exports.respond = function(event, cb) {
+var AWS = require('aws-sdk');
+var docClient = new AWS.DynamoDB.DocumentClient({
+  region: AWS.config.region || 'eu-west-1'
+});
+var table = process.env.SERVERLESS_DATA_MODEL_STAGE + '-blog';
 
-  var response = {
-    message: "Your Serverless function ran successfully!"
-  };
+module.exports.respond = function (event, cb) {
 
-  return cb(null, response);
+  switch (event.method) {
+    case 'GET':
+      getPosts(cb);
+      break;
+    case 'POST':
+      var post = event.body;
+      post.id = null;
+      savePost(post, cb);
+      break;
+    case 'PUT':
+      var post = event.body;
+      post.id = event.id;
+      savePost(post, cb);
+      break;
+    case 'DELETE':
+      deletePost(event.id, cb);
+      break;
+    default:
+      cb("Unkown method " + event.method);
+  }
 };
+
+function getPosts(cb) {
+  var params = {
+    TableName: table,
+    AttributesToGet: [
+      'id',
+      'title',
+      'content',
+      'date'
+    ]
+  };
+  docClient.scan(params, cb);
+}
+
+function savePost(post, cb) {
+  post.id = post.id ? post.id : Date.now().toString();
+  var params = {
+    TableName: table,
+    Item: post
+  };
+  docClient.put(params, function (err, response) {
+    if (err) {
+      cb(err, response)
+    }
+    cb(null, {
+      message: "Post successfully saved !",
+      post: post
+    });
+  });
+}
+
+function deletePost(postId, cb) {
+  var params = {
+    TableName: table,
+    Key: {id: postId}
+  };
+  docClient.delete(params, function (err, response) {
+    if (err) {
+      cb(err, response);
+    }
+    cb(null, {message: "Post successfully deleted !", id: postId});
+  });
+}
